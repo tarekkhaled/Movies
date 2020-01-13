d65ee57b965f40099014c392b892884a0bb94bcb
tarek tito <tarekt820@gmail.com>
2020-01-13 23:51:05 +0200

    done with outer structure next will be code for model and controllers

--- src/resources/movies/movies.controllers.js
+++ src/resources/movies/movies.controllers.js
@@ -1,1 +1,33 @@
-// controllers of movies api
+import Movie from './movies.model';
+const MovieModel = new Movie ;
+
+/** controller for api (get '/movies/') */
+export const getAllMovies = (req,res,next) => {
+
+}
+
+/** controller for api (get '/movies/topRated') */
+export const getTopRatedMovies = (req,res,next) => {
+
+}
+
+/** controller for api (get '/movies/:id') */
+export const getOneMovie = (req,res,next) => {
+
+}
+
+/** controller for api (post '/movies/') */
+export const addNewMovie = (req,res,next) => {
+
+}
+
+/** controller for api (put '/movies/:id') */
+export const updateMovie = (req,res,next) => {
+
+}
+/** controller for api (delete '/movies/:id') */
+export const deleteMovie = (req,res,next) => {
+
+}
+
+
--- src/resources/movies/movies.model.js
+++ src/resources/movies/movies.model.js
@@ -1,1 +1,20 @@
 // model for movies that have to deal with the database
+
+class Movie {
+    getAllMovies(){    
+    }
+    getMovieByID(id){
+    }
+    getTopRatedMovie(){
+    }
+    addNewMovie(movie){   
+    }
+    updateMovieByID(id){
+    }
+    deleteMovieByID(id){
+    }
+}
+
+
+
+export default Movie;
