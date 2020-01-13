d523f7c404df89b59c051b7a5a8dc8146573dc1e
tarek tito <tarekt820@gmail.com>
2020-01-13 23:50:16 +0200

    done with the logic of movies routers (api)

--- src/resources/movies/movies.routers.js
+++ src/resources/movies/movies.routers.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const router = express.Router();
+import {
+    getAllMovies,
+    getOneMovie,
+    getTopRatedMovies,
+    addNewMovie,
+    updateMovie,
+    deleteMovie} 
+from './movies.controllers';
 
-// just a placeholder and i will replace it later
-const controller = (req,res,next) => {
-    res.json({name : 'adel shakel'})
-}
 // for route /movies/
 router.route('/')
-    .get(controller) // return list of movies with pages
-    .post(controller) // will add the movie to the list
+    .get(getAllMovies) // return list of movies with pages
+    .post(addNewMovie) // will add the movie to the list
 
 // for route /movies/top_rated
 router.route('/top_rated')
-    .get(controller) // will get top_rated 
+    .get(getTopRatedMovies) // will get top_rated 
 
 // for route /movies/:id
 router.route('/:movieID')
-    .get(controller) // will just return one movie -- R
-    .put(controller) // will just update the movie -- U
-    .delete(controller) // will delete the movie -- D
+    .get(getOneMovie) // will just return one movie -- R
+    .put(updateMovie) // will just update the movie -- U
+    .delete(deleteMovie) // will delete the movie -- D
 
 
 module.exports = router;
